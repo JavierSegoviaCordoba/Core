a8a16f47b243368228bf53a63972f5d7b8d1857d
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2020-06-19 20:50:21 +0200

    Add Merger to combine remote and local data sources

--- buildSrc/src/main/kotlin/CommonMain.kt
+++ buildSrc/src/main/kotlin/CommonMain.kt
@@ -3,6 +3,8 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinDependencyHandler
 val KotlinDependencyHandler.commonMainDependencies: List<String>
     get() = with(libs) {
         listOf(
-            kotlinStdlibCommon
+            kotlinStdlibCommon,
+            networkResponse,
+            resource
         )
     }
