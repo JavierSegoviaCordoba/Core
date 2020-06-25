ea2d8fc442828e43f773241404b18650ef8ce013
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2020-06-25 22:43:59 +0200

    Remote fetching is now oneshot instead of flow

--- core/src/commonMain/kotlin/com/javiersc/resources/core/data/RepoFlow.kt
+++ core/src/commonMain/kotlin/com/javiersc/resources/core/data/RepoFlow.kt
@@ -4,12 +4,11 @@ import com.javiersc.resources.resource.Resource
 import com.javiersc.resources.resource.extensions.ifError
 import com.javiersc.resources.resource.extensions.ifSuccess
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.flow
 
 inline fun <reified R, reified E> repoFlow(
-    crossinline getFromRemote: () -> Flow<Resource<R, E>>,
+    crossinline getFromRemote: suspend () -> Resource<R, E>,
     crossinline getFromLocal: () -> Flow<Resource<R, E>>,
     crossinline saveToLocal: suspend (R) -> Unit,
     localOnStart: Boolean = false,
@@ -19,11 +18,9 @@ inline fun <reified R, reified E> repoFlow(
 
     if (loadingOnStart) emit(Resource.Loading)
 
-    getFromRemote().collect { resource ->
-        resource.apply {
-            ifError { emit(resource) }
-            ifSuccess { data -> saveToLocal(data) }
-        }
+    getFromRemote().apply {
+        ifError { emit(this) }
+        ifSuccess { data -> saveToLocal(data) }
     }
     if (!localOnStart) emitAll(getFromLocal())
 }
